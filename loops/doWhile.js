b9b62953799a56e887e269e940563f3f7f0296a9
Niru-26016 <niru26016@gmail.com>
2025-10-05 22:18:32 +0530

    added Q2 in do while loop

--- loops/doWhile.js
+++ loops/doWhile.js
@@ -13,3 +13,14 @@ do {
   }
 } while (game.toUpperCase() != "STOP");
 console.log(gameCollection);
+
+//2. write a do while loop that adds numbers from 1 to 3 and stores the result in a variable named "totle".
+
+let total=0;
+let i=1;
+do {
+    total+=i;
+    i++;
+} while (i<=3);
+console.log(total);
+
