62cd93d083ec2b3e79f37df9dd616ea8a6d17c04
Niru-26016 <niru26016@gmail.com>
2025-10-05 20:38:16 +0530

    added while loop 2Q

--- loops/whileLoop.js
+++ loops/whileLoop.js
@@ -1,3 +1,4 @@
+//1. While loop that calculate the sum of all numbers from 1 to 5
 let sum = 0;
 i = 1;
 while (i <= 5) {
@@ -5,3 +6,15 @@ while (i <= 5) {
   i++;
 }
 console.log(sum);
+
+//2. Write a while loop that counts down from 5 to 1 and stores the variable in an array
+
+let countdown = [];
+let j = 5;
+while (j > 0) {
+  console.log(j);
+  countdown.push(j);
+  j--;
+}
+console.log(countdown);
+
