6f8a084b4e9000d33acf1b9f33a87d21d546e430
Niru-26016 <niru26016@gmail.com>
2025-10-09 13:35:17 +0530

    added more in for.js

--- loops/for.js
+++ loops/for.js
@@ -8,3 +8,29 @@ for (let i = 0; i < numbers.length; i++) {
 }
 console.log(multipliedNumber);
 
+//2. write a for loop that loops through the array "["GTA V","COD","RDR","GTA VI","PUBG","FIFA"]" and stops the loop when it finds "GTA VI" .
+// stores all games before "GTA VI" in s new array
+
+let games = ["GTA V", "COD", "RDR", "GTA VI", "PUBG", "FIFA"];
+let gameCollection = [];
+for (let j = 0; j < games.length; j++) {
+  if (games[j] === "GTA VI") {
+    break;
+  }
+  gameCollection.push(games[j]);
+}
+console.log(gameCollection);
+
+
+//3. write a for loop that loops through the array "["GTA V","COD","RDR","GTA VI","PUBG","FIFA"]" and skipes "GTA VI" .
+// stores the other games in playedGames
+
+// let games = ["GTA V", "COD", "RDR", "GTA VI", "PUBG", "FIFA"];
+let playedGames = [];
+for (let i = 0; i < games.length; i++) {
+  if (games[i] === "GTA VI") {
+    continue;
+  }
+  playedGames.push(games[i]);
+}
+console.log(playedGames);
