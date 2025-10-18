4085636befc9e5ba31b750fe8ff478e9fbfb42b2
Niru-26016 <niru26016@gmail.com>
2025-10-18 12:19:33 +0530

    updated find max

--- Challenges/Arrays And Methods/Find Maximum.js
+++ Challenges/Arrays And Methods/Find Maximum.js
@@ -1,13 +1,6 @@
 // Write a function findMax(arr) that returns the largest number in the array
 
 function findMax(arr) {
-  let max=0;
-  arr.forEach(element => {
-    if(element>max){
-        max=element;
-    }
-  });
-  return max;
+  return Math.max(...arr);
 }
-
 console.log(findMax([1, 2, 3, 4, 5]));
