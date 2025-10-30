c7febfc1602dbd950863e0e65d14b05306f3cf8f
Niru-26016 <niru26016@gmail.com>
2025-10-30 22:47:40 +0530

    added Prototype

--- oop/Prototype.js
+++ oop/Prototype.js
@@ -1,4 +1,15 @@
-let car ={
-    
-
+let Human ={
+    legs:2,
+    hands:2,
+    eyes:2,
 }
+
+let men={
+    hair:"Shot hair",
+}
+
+Object.setPrototypeOf(men,Human);
+
+console.log(Object.getPrototypeOf(men));
+console.log(men);
+console.log(men.hands);
